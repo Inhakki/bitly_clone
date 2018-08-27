e3a57f43cf163af16e34488f7811c700ecc50f49
inhakki <inhakki@gmail.com>
2018-08-27 00:00:07 -0400

    Added sidekiq and names for controller actions.

---
+++ app/controllers/link_controller.rb
@@ -0,0 +1,22 @@
+class LinkController < ApplicationController
+
+  def determine_route
+    if Link.where(short_code: params[:short_code]).exists?
+
+    else
+
+    end
+  end
+
+  def create_link
+
+  end
+
+  def redirect_link
+
+  end
+
+  def return_sorted_link_list
+    
+  end
+end
